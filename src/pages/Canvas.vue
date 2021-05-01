9451cedd32484d5f72ad55eb23ddfb68e30c9b51
Jose David <david_gh86@hotmail.es>
2021-05-01 21:44:50 +0200

    switch photo cartoon

--- src/pages/Canvas.vue
+++ src/pages/Canvas.vue
@@ -3,17 +3,22 @@
     <div class="row">
       <div id="overlay">
         <!-- <img :src="imageFile" style="position:absolute; top:0; left:0;"> -->
-        <div id="overin" :style="{ backgroundImage: 'url(\'' + imageFile + '\')' }"></div>
+        <div id="overin" v-bind:style="{ 'background-image': 'url(\'' + imageFile + '\')' }"></div>
       </div>
     </div>
-      <div class="row">
-        <q-btn-toggle
-        v-model="selectedColor"
-        toggle-color="primary"
-        push
-        glossy
-        :options="buttonColors"
-      ></q-btn-toggle> {{ selectedColor }}
+    <div class="row">
+      <q-btn-toggle
+      v-model="selectedColor"
+      toggle-color="primary"
+      push
+      glossy
+      :options="buttonColors"
+    >
+      </q-btn-toggle> {{ selectedColor }}
+    </div>
+    <div class="row">
+      <q-btn color="primary" label="Paint" v-on:click="paintAll"></q-btn>
+      <q-btn color="primary" label="Photo/Cartoon" v-on:click="switchPhoto"></q-btn>
     </div>
   </div>
 </template>
@@ -27,30 +32,35 @@ export default {
 
   data() {
     return {
-      imageFile: require("../assets/451542_16.jpg"),
+      imageFile: "",
+      imageUid: "",
       titlea: 'Misco',
       svg: '',
       dictColors: {},
       dictColorsIndex: [],
       buttonColors: [],
-      selectedColor: ""
+      selectedColor: "",
+      type: "cartoon"
     };    
   },
   watch: {
     selectedColor: function (newColor, oldColor){
       if (oldColor){
-        let oldElements = document.querySelectorAll('[blockColorCode="'+ oldColor +'"][blockColorPainted=false]')
+        let oldElements = this.getNotPaintedBlocksByColor(oldColor)
         for (let element of oldElements){
           this.unselectElement(element)
         }
       }
-      let newElements = document.querySelectorAll('[blockColorCode="'+ newColor +'"][blockColorPainted=false]')
+      let newElements = this.getNotPaintedBlocksByColor(newColor)
       for (let element of newElements){
         this.selectElement(element)
       }
     }
   },
   methods: {
+    getNotPaintedBlocksByColor(color){
+      return document.querySelectorAll('[blockColorCode="'+ color +'"][blockColorPainted=false]')
+    },
     unselectElement: function(element){
       element.setAttribute("fill", "rgb(255,255,255)")
       element.setAttribute("stroke", "rgb(255,255,255)")
@@ -63,6 +73,9 @@ export default {
     },
     onClickBlockEvent: function(event){
       let element = event.target
+      this.colour(element)
+    },
+    colour: function(element){
       element.setAttribute("opacity", 0)
       element.setAttribute("blockColorPainted", true)
       'use strict';
@@ -75,17 +88,21 @@ export default {
       for (let i=0; i<childNodes.length; i++){
         let childNode = childNodes[i]
         childNode.setAttribute("blockId", i)
-        let color = childNode.attributes["fill"].value
-       childNode.setAttribute("blockColorCode", color)
-        childNode.setAttribute("blockColorPainted", false)
-        if (!this.dictColors[color]){
-          this.dictColors[color] = []
-          this.dictColorsIndex.push(color)
-        }else{
-          this.dictColors[color].push(i)
+        let color = childNode.getAttribute("fill")
+        childNode.setAttribute("blockColorCode", color)
+        if (color != "rgb(0,0,0)"){
+          childNode.setAttribute("blockColorPainted", false)
+          if (!this.dictColors[color]){
+            this.dictColors[color] = []
+            this.dictColorsIndex.push(color)
+          }else{
+            this.dictColors[color].push(i)
+          }
+          childNodes[i].setAttribute("fill", "rgb(255,255,255)")
+          childNodes[i].setAttribute("stroke", "rgb(255,255,255)")
+        } else {
+          childNode.setAttribute("blockColorPainted", true)
         }
-        childNodes[i].setAttribute("fill", "rgb(255,255,255)")
-        childNodes[i].setAttribute("stroke", "rgb(255,255,255)")
       }
       this.initButtonColors()
     },
@@ -99,23 +116,45 @@ export default {
         result.push(item)
       }
       this.buttonColors = result
+    },
+    paintAll: async function() {
+      // TODO fix this
+      for (let i=0; i<this.dictColorsIndex.length; i++){
+        this.selectedColor = this.dictColorsIndex[i]
+        await this.paintByColour(this.dictColorsIndex[i])
+      }
+    },
+    paintByColour: async function(color) {
+      let blocksByColor = this.getNotPaintedBlocksByColor(color)
+      setTimeout(()=> {
+        for (let element of blocksByColor){
+          this.colour(element)
+        }
+      }, 1000)
+    },
+    switchPhoto: function() {
+      if (this.type == "cartoon"){
+        this.imageFile = "http://localhost:5000/photo/"+this.imageUid
+        this.type = "photo"
+      }else {
+        this.imageFile = "http://localhost:5000/cartoon/"+this.imageUid
+        this.type = "cartoon"
+      }
     }
   },
   created () {
     console.log('2 - created')
   },
-  mounted () {
+  beforeMount () {
     let imageId = this.$route.params.id
+    this.imageUid = imageId
 
     this.imageFile = "http://localhost:5000/cartoon/"+imageId
+    this.type = "cartoon"
 
     this.$axios.get("svg/"+imageId).then(response => {
       this.loadSvg(response.data)
     })
-
-    
-    
-    //imagetracer_v126.imageToSVG(this.imageFile, this.loadSvg, {numberofcolors : 16, mincolorratio:20})
   }
 }
 </script>
